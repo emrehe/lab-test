36417eaa6d344cd79422d932bc8d747cb23562fb
emrehe <emre.he@gmail.com>
2015-03-17 00:07:53 +0800

    initial commit

---
+++ src/main/java/com/lab/servlet/TomService.java
@@ -0,0 +1,35 @@
+package com.lab.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+
+@Path("/tom")
+public class TomService
+{
+    
+    @POST
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String test()
+    {
+    	JsonObject json = new JsonObject();
+    	json.addProperty("name", "tom");
+        return json.toString();
+    }
+
+
+}
